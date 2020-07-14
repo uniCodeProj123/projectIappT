b7a6ab4a1146da5afbbcbabf5248df5022482975
Hussain <anas.hussain@capgemini.com>
2020-07-14 11:48:54 +0100

    authors

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
@@ -13,7 +16,11 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology:
@@ -21,7 +28,6 @@ true, useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-
 
 
 app.listen(process.env.PORT || 3000)
