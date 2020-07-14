1ca88a28a2c8f0e4890cab16aa6b4b8c8c6cfb1b
Hussain <anas.hussain@capgemini.com>
2020-07-14 12:01:59 +0100

    small fix 3000

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 3000)
