f72ddee5b5e1cf650800fa410ef53203947a1ee6
Hussain <anas.hussain@capgemini.com>
2020-07-17 17:10:42 +0100

    books

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname+'/views')
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 const mongoose = require('mongoose')
